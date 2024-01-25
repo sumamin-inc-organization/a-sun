046b98369aeabb06a68e01010d3ee814adfa60e2
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-01-25 12:58:02 +0900

    画像用のフォルダーを作成し、ヘッダーとヒーローセクションに必要なすべての画像を追加しました。

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import "./assets/css/common/reset.css";
 import "./assets/css/common/typography.css";
 import "./assets/css/common/utility.css";
-import "./assets/css/home/header.css";
-import "./assets/css/home/nav.css";
+import "./assets/css/common/header.css";
+import "./assets/css/common/nav.css";
+import "./assets/css/home/top-nav.css";
+import "./assets/css/home/hero.css";
+import "./assets/css/home/feature.css";
+import "./assets/css/home/course.css";
+import "./assets/css/home/lunch.css";
+import "./assets/css/home/dinner.css";
+import "./assets/css/home/message.css";
+import "./assets/css/home/voice.css";
+import "./assets/css/home/news.css";
+import "./assets/css/home/insta.css";
+import "./assets/css/home/access.css";
+
+
+
+
+
