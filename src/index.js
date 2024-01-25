f17c4fbf66dd6998905d6b645f4b1cd22a472968
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-01-25 12:49:02 +0900

    トップページに必要なCSSファイルを作成し、インポートしました。

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import "./assets/css/style.css";
+import "./assets/css/common/reset.css";
+import "./assets/css/common/typography.css";
+import "./assets/css/common/utility.css";
+import "./assets/css/home/header.css";
+import "./assets/css/home/nav.css";
