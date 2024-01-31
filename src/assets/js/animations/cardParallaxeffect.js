4996a98bb169d099fcc133cd6dad1fced96321f7
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-01-31 19:13:56 +0900

    PCまたはタブレットかどうかを確認するスクリプトを記述しました。

--- src/assets/js/animations/cardParallaxeffect.js
+++ src/assets/js/animations/cardParallaxeffect.js
@@ -1,18 +1,19 @@
 
 
-// import { gsap } from "gsap";
-// gsap.registerPlugin(ScrollTrigger);
-// export default function activateCardParallax(){
-//     let images = gsap.utils.toArray(".parallax");
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+export default function activateCardParallax(){
+    let images = gsap.utils.toArray(".parallax");
 
-//     images.forEach((image) => {
-//       gsap.to(image, {
-//         yPercent: -100 * image.dataset.speed,
-//         ease: "none",
-//         scrollTrigger: {
-//           scrub: image.dataset.speed,
-//         },
-//       });
-//     });
+    images.forEach((image) => {
+      gsap.to(image, {
+        yPercent: -100 * image.dataset.speed,
+        ease: "none",
+        scrollTrigger: {
+          scrub: image.dataset.speed,
+        },
+      });
+    });
     
-// }
+}
