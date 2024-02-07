a13876326b2cca2155d1e3f1fe95631f73a72958
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-02-07 18:25:27 +0900

    top-nav.cssをanimations.cssに名前変更しました。

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./assets/css/common/typography.css";
 import "./assets/css/common/utility.css";
 import "./assets/css/common/header.css";
 import "./assets/css/common/nav.css";
-import "./assets/css/home/top-nav.css";
+import "./assets/css/home/animations.css";
 import "./assets/css/home/hero.css";
 import "./assets/css/home/feature.css";
 import "./assets/css/home/course.css";
