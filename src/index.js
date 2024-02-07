7758776283a34c9f3014458852a39318d587ce8d
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-02-07 19:22:07 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -154,10 +154,11 @@ document.addEventListener('DOMContentLoaded',(e)=>{
   intro
 .from('.transition-item',{y:'100%',delay:2})
 .from('.transition-item-white',{y:'100%'})
-.set('.transition-item',{y:'-100%'})
+// .set('.transition-item',{y:'-100%'})
 .set('.transition-img',{opacity:0})
 .set('.transition-img',{x:"100%" ,})
 .to('.transition-item-white',{y:'-100%'})
 .to('.transition-item-white',{display:"none"})
+.set('.transition-item',{display:"none"})
 
 })
