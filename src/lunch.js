55f3431fc2764386e46746964c079b4eb5d04abe
Shoma Kumagawa <s.kumagawa@sumamin.com>
2024-02-08 19:00:07 +0900

    lunchページpc版作成（drinkセクション以外）

--- src/lunch.js
+++ src/lunch.js
@@ -2,7 +2,9 @@ import "./assets/css/common/reset.css";
 import "./assets/css/common/typography.css";
 import "./assets/css/common/utility.css";
 import "./assets/css/common/header.css";
+import "./assets/css/common/footer.css";
 import "./assets/css/common/nav.css";
+import "./assets/css/home/access.css";
 import "./assets/css/lunch/lunch.css";
 
 
