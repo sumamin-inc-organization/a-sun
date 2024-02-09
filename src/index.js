afa3178863ca614c9cee32b1de7f3c53aa0241a6
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-02-09 15:05:29 +0900

    ローディングアニメーション中にヘッダーが表示される問題を修正しました

--- src/index.js
+++ src/index.js
@@ -180,3 +180,28 @@ function animateLogo() {
   tween.restart();
 }
 
+
+/*-------------------------------
+   Header PC animation
+   ヘッダーPCアニメーション
+-------------------------------*/
+
+let pcHeaderAnim = gsap.matchMedia();
+
+pcHeaderAnim.add("(min-width: 800px)", () => {
+
+  const showAnim = gsap.from('.header', { 
+    yPercent: -100,
+    paused: true,
+    duration: 0.2
+  }).progress(1);
+  
+  
+  ScrollTrigger.create({
+    start: "top top",
+    end: "max",
+    onUpdate: (self) => {
+      self.direction === -1 ? showAnim.play() : showAnim.reverse()
+    }
+  });
+});
