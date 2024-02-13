e1376841b411bf4899a353dc38d8a498059a6093
kumagawa shoma <s.kumagawa@sumamin.com>
2024-02-13 18:03:50 +0900

    news一覧ページ作成

---
+++ src/news.js
@@ -0,0 +1,14 @@
+import "./assets/css/common/reset.css";
+import "./assets/css/common/typography.css";
+import "./assets/css/common/utility.css";
+import "./assets/css/common/header.css";
+import "./assets/css/common/footer.css";
+import "./assets/css/common/nav.css";
+import "./assets/css/home/animations.css";
+import "./assets/css/home/access.css";
+import "./assets/css/news/news.css";
+import setLoadingAnimation from "./assets/js/animations/loadingAnimation";
+
+import { gsap } from "gsap";
+setLoadingAnimation();
+
