cca518f44966a091d52b22493c3ee2a7c9f9f1cd
kumagawa shoma <s.kumagawa@sumamin.com>
2024-02-14 14:18:52 +0900

    lunch, dinnerページ出現アニメーション作成

--- src/dinner.js
+++ src/dinner.js
@@ -15,6 +15,9 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 setLoadingAnimation();
 
 /* slick animation */
@@ -29,5 +32,111 @@ var $slide = $(".slick-slide")
     slidesToScroll: 1,
     infinite: true,
     dots: true,
-    // adaptiveHeight:true
+});
+
+/*  Appearance Animation */
+let mm = gsap.matchMedia();
+/* sp */
+mm.add("(max-width: 767px)", () => {
+    // overview content
+    let overview = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".overview-grid",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    overview
+    .from(".overview-description", { y: 30, opacity: 0, duration: 1.5}, "+=2.5")
+    .from(".overview-hours", { y: 30, opacity: 0, duration: 1.5}, "-=0.8")
+
+    // lunchCourse content
+    let lunchCourse = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#lunchCourse",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    lunchCourse
+    .from(".course-headline", { y: 30, opacity: 0, duration: 1.2}, "+=0.5")
+    .from(".lunch-content:nth-child(1)", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+    .from(".lunch-content:nth-child(2)", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+    .from(".lunch-content:nth-child(3)", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+
+    // drink
+    let drink = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#drink",
+            start: "-=650px",
+            scrub: false,
+        },
+    });
+    drink
+    .from(".drink-headline", { y: 30, opacity: 0, duration: 1.2}, "+=0.5")
+    .from(".drink-inner", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+
+    // home btn
+    let home_btn = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".home-btn",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    home_btn
+    .from(".home-btn a", { y: 30, opacity: 0, duration: 1})
+});
+
+/* pc */
+mm.add("(min-width: 768px)", () => {
+    // overview content
+    let overview = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".overview-grid",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    overview
+    .from(".overview-description", { y: 30, opacity: 0, duration: 1.5}, "+=2.5")
+    .from(".overview-hours", { y: 30, opacity: 0, duration: 1.5}, "-=0.8")
+
+    // lunchCourse content
+    let lunchCourse = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#lunchCourse",
+            start: "-=600px",
+            scrub: false,
+        },
+    });
+    lunchCourse
+    .from(".course-headline", { y: 30, opacity: 0, duration: 1.5}, "+=0.5")
+    .from(".lunch-content:nth-child(1)", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+    .from(".lunch-content:nth-child(2)", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+    .from(".lunch-content:nth-child(3)", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+
+    // drink
+    let drink = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#drink",
+            start: "-=650px",
+            scrub: false,
+        },
+    });
+    drink
+    .from(".drink-headline", { y: 30, opacity: 0, duration: 1.5}, "+=0.5")
+    .from(".drink-inner", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+
+    // home btn
+    let home_btn = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".home-btn",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    home_btn
+    .from(".home-btn a", { y: 30, opacity: 0, duration: 1})
+
 });
--- src/lunch.js
+++ src/lunch.js
@@ -15,6 +15,9 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 setLoadingAnimation();
 
 /* slick animation */
@@ -29,5 +32,140 @@ var $slide = $(".slick-slide")
     slidesToScroll: 1,
     infinite: true,
     dots: true,
-    // adaptiveHeight:true
+});
+
+
+/*  Appearance Animation */
+let mm = gsap.matchMedia();
+/* sp */
+mm.add("(max-width: 767px)", () => {
+    // overview content
+    let overview = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".overview-grid",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    overview
+    .from(".overview-description", { y: 30, opacity: 0, duration: 1.5}, "+=2.5")
+    .from(".overview-hours", { y: 30, opacity: 0, duration: 1.5}, "-=0.8")
+    .from(".overview-buttons", { y: 30, opacity: 0, duration: 1.5}, "-=0.8")
+
+    // lunchCourse content
+    let lunchCourse = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#lunchCourse",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    lunchCourse
+    .from(".course-headline", { y: 30, opacity: 0, duration: 1.2}, "+=0.5")
+    .from(".course-content:nth-child(1)", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+    .from(".course-content:nth-child(2)", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+    .from(".course-content:nth-child(3)", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+
+    // lunchSingle
+    let lunchSingle = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#lunchSingle",
+            start: "-=620px",
+            scrub: false,
+        },
+    });
+    lunchSingle
+    .from(".single-headline", { y: 30, opacity: 0, duration: 1.2}, "+=0.5")
+    .from(".single-content:nth-child(1)", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+    .from(".single-content:nth-child(2)", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+
+    // drink
+    let drink = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#drink",
+            start: "-=650px",
+            scrub: false,
+        },
+    });
+    drink
+    .from(".drink-headline", { y: 30, opacity: 0, duration: 1.2}, "+=0.5")
+    .from(".drink-inner", { y: 30, opacity: 0, duration: 1.2}, "-=0.5")
+
+    // home btn
+    let home_btn = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".home-btn",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    home_btn
+    .from(".home-btn a", { y: 30, opacity: 0, duration: 1})
+});
+
+/* pc */
+mm.add("(min-width: 768px)", () => {
+    // overview content
+    let overview = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".overview-grid",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    overview
+    .from(".overview-description", { y: 30, opacity: 0, duration: 1.5}, "+=2.5")
+    .from(".overview-hours", { y: 30, opacity: 0, duration: 1.5}, "-=0.8")
+    .from(".overview-buttons", { y: 30, opacity: 0, duration: 1.5}, "-=0.8")
+
+    // lunchCourse content
+    let lunchCourse = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#lunchCourse",
+            start: "-=500px",
+            scrub: false,
+        },
+    });
+    lunchCourse
+    .from(".course-headline", { y: 30, opacity: 0, duration: 1.5}, "+=0.5")
+    .from(".course-content:nth-child(1)", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+    .from(".course-content:nth-child(2)", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+    .from(".course-content:nth-child(3)", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+
+    // lunchSingle
+    let lunchSingle = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#lunchSingle",
+            start: "-=650px",
+            scrub: false,
+        },
+    });
+    lunchSingle
+    .from(".single-headline", { y: 30, opacity: 0, duration: 1.5}, "+=0.5")
+    .from(".single-content:nth-child(1)", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+    .from(".single-content:nth-child(2)", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+
+    // drink
+    let drink = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#drink",
+            start: "-=650px",
+            scrub: false,
+        },
+    });
+    drink
+    .from(".drink-headline", { y: 30, opacity: 0, duration: 1.5}, "+=0.5")
+    .from(".drink-inner", { y: 30, opacity: 0, duration: 1.5}, "-=0.5")
+
+    // home btn
+    let home_btn = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".home-btn",
+            start: "-=550px",
+            scrub: false,
+        },
+    });
+    home_btn
+    .from(".home-btn a", { y: 30, opacity: 0, duration: 1})
+
 });
