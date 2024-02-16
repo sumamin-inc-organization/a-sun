711ecce20499a5869c131fb6a52b38c070fda039
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-02-16 13:29:09 +0900

    news info  ページにアクセスセクションのアニメーションを追加しました。

--- src/news_details.js
+++ src/news_details.js
@@ -13,6 +13,8 @@ import { gsap } from "gsap";
 setLoadingAnimation();
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { toggleSpMenu } from "./assets/js/eventlisters/toggleSpMenu";
+import { activateAccessTitleAnimationsFeature } from "./assets/js/animations/activateAccessTitleAnimations";
+
 gsap.registerPlugin(ScrollTrigger);
 
 toggleSpMenu();
@@ -48,3 +50,6 @@ let home_btn = gsap.timeline({
   },
 });
 home_btn.from(".home-btn", { y: 30, opacity: 0, duration: 2 });
+
+
+activateAccessTitleAnimationsFeature();
