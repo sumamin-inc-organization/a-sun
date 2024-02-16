b1ebba1befd1dd54d99631d5f321631001ddef2e
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-02-16 13:05:20 +0900

    アクセスのアニメーション用にディナーセクションのバージョンを作成しました。

--- src/dinner.js
+++ src/dinner.js
@@ -18,6 +18,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { toggleSpMenu } from "./assets/js/eventlisters/toggleSpMenu";
 import activateHeaderAnimations from "./assets/js/animations/activateHeaderAnimations";
+import activateAccessTitleAnimations, { activateAccessTitleAnimationsDinner } from "./assets/js/animations/activateAccessTitleAnimations";
 gsap.registerPlugin(ScrollTrigger);
 
 setLoadingAnimation();
@@ -182,3 +183,4 @@ mm.add("(min-width: 768px)", () => {
 });
 
 activateHeaderAnimations();
+activateAccessTitleAnimationsDinner();
