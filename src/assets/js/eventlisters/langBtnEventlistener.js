1d6c97f6b4727c7a862be7fddd765df25763ef8c
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-02-20 13:04:39 +0900

    言語ボタンがクリックされたときに選択された言語と言語ボタンのテキストを変更するJavaScriptを記述しました。

--- src/assets/js/eventlisters/langBtnEventlistener.js
+++ src/assets/js/eventlisters/langBtnEventlistener.js
@@ -1,35 +1,30 @@
 import { selector } from "gsap";
 import GetStorageItems from "../otherlogic/getStorage";
 import updateLangBtnText from "../otherlogic/updateLangBtnText";
+import setStorageItem from "../otherlogic/setStorage";
 
-
-function determineBtnClicked(element){
-   if( element.dataset.languageSelector === "jp"){
-    console.log("jp");
+function determineBtnClicked(element,key) {
+    const LANGUAGE = element.dataset.languageSelector;
+    setStorageItem(key,LANGUAGE);
+  if (LANGUAGE === "jp") {
     updateLangBtnText("jp");
-   }
-   else if( element.dataset.languageSelector === "en"){
+  } else if (LANGUAGE === "en") {
     updateLangBtnText("en");
-   }
+  }
 }
 
+export default function activateLangBtnEventListener() {
+  const btn = document.querySelector(".dropdown__btn");
+  let SELECTORS = document.querySelectorAll("[data-language-selector]");
+  let JP_BTN = document.querySelector('[data-language-selector="jp"]');
+  let EN_BTN = document.querySelector('[data-language-selector="en"]');
+  const LOCAL_STORAGE_LANGUAGE_KEY = "asun.lang";
+  let currentLang = GetStorageItems(LOCAL_STORAGE_LANGUAGE_KEY);
 
-
-export default function activateLangBtnEventListener(){
-    const btn = document.querySelector('.dropdown__btn');
-    let SELECTORS = document.querySelectorAll('[data-language-selector]')
-    let JP_BTN = document.querySelector('[data-language-selector="jp"]')
-    let EN_BTN = document.querySelector('[data-language-selector="en"]')
-    const LOCAL_STORAGE_LANGUAGE_KEY = "asun.lang" ;
-    let currentLang = GetStorageItems(LOCAL_STORAGE_LANGUAGE_KEY);
-
-        // updateLangBtnText(currentLang);
-
-    SELECTORS.forEach(selector => {
-        selector.addEventListener("click",(e)=>{
-            const clicked = e.target; 
-            determineBtnClicked(clicked);
-            
-        })
+  SELECTORS.forEach((selector) => {
+    selector.addEventListener("click", (e) => {
+      const clicked = e.target;
+      determineBtnClicked(clicked,LOCAL_STORAGE_LANGUAGE_KEY);
     });
+  });
 }
