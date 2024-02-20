f7826a9c1975944c8b6bc2f092816e9620787c9f
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-02-20 16:18:58 +0900

    言語切り替えを機能させました。

--- src/assets/js/otherlogic/toggleLanguage.js
+++ src/assets/js/otherlogic/toggleLanguage.js
@@ -5,16 +5,20 @@ const ALL = document.querySelectorAll('[data-lang]')
 
 function removeClasses(){
     ALL.forEach(element =>{
-        element.classList.remove('visible');
+        element.classList.remove('hidden');
     })
 }
 
 export default function toggleLanguage(current){
     removeClasses();
-    if (current === "jp" && element.dataset.mainLang === "cn") {
-        element.classList.add('hidden'); // Add 'visible' class to elements with lang "jp" if current is "jp"
-    } else if (current === "en" && element.dataset.mainLang === "jp") {
-        element.classList.add('hidden'); // Add 'visible' class to elements with lang "en" if current is "en"
-    }
+    const ALL = document.querySelectorAll('[data-lang]')
+    ALL.forEach(element =>{
+        if (current === "jp" && element.dataset.lang === "en") {
+            element.classList.add('hidden'); // Add 'hidden' class to elements with lang "jp" if current is "jp"
+        } else if (current === "en" && element.dataset.lang === "jp") {
+            element.classList.add('hidden'); // Add 'hidden' class to elements with lang "en" if current is "en"
+        }
+    })
+   
 
 }
